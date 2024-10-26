6690d999d6a2e390073bfcbec2e962500e4e4171
David Taylor <officialdavidtaylor@gmail.com>
2024-10-26 12:34:16 -0700

    housekeeping: minor tweaks/fixes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,7 +2,7 @@ import type { MetaFunction } from '@remix-run/node';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: 'Leave by Nine club' },
+    { title: 'Lb9_club' },
     {
       name: 'description',
       content: 'A super secret event network for friends',
