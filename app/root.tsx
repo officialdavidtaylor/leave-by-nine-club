19149f8e4f041c9b108fe9f3da2dc9afa364b538
David Taylor <officialdavidtaylor@gmail.com>
2024-10-26 12:34:16 -0700

    housekeeping: minor tweaks/fixes

--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,10 @@ import type { LinksFunction } from '@remix-run/node';
 import './tailwind.css';
 
 export const links: LinksFunction = () => [
+  {
+    rel: 'icon',
+    href: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌇</text></svg>',
+  },
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
   {
     rel: 'preconnect',
