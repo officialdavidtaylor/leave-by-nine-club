994d13bc06ef6449c482d9877fde24c4edf4115f
David Taylor <officialdavidtaylor@gmail.com>
2024-10-28 08:30:04 -0700

    protect env variable usage with assertions

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,14 @@
+import assert from 'assert';
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
+
+assert(process.env.VITE_DB_FILE_NAME, 'DB_FILE_NAME is undefined');
 
 export default defineConfig({
   out: './db/drizzle',
   schema: './db/schema.ts',
   dialect: 'sqlite',
   dbCredentials: {
-    url: process.env.DB_FILE_NAME!,
+    url: process.env.VITE_DB_FILE_NAME,
   },
 });
