52c11b50fd31ea0b804e46472b56c4eb38782808
David Taylor <officialdavidtaylor@gmail.com>
2024-10-28 08:30:04 -0700

    protect env variable usage with assertions

--- db/index.server.ts
+++ db/index.server.ts
@@ -1,4 +1,7 @@
+import assert from 'assert';
 import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/libsql';
+import { drizzle } from 'drizzle-orm/libsql/node';
 
-export const db = drizzle(process.env.DB_FILE_NAME!);
+assert(process.env.VITE_DB_FILE_NAME, 'VITE_DB_FILE_NAME is undefined');
+
+export const db = drizzle(process.env.VITE_DB_FILE_NAME);
