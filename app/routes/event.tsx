9c314957f294146241f750af0a0bb8f5e2a35a74
David Taylor <officialdavidtaylor@gmail.com>
2024-10-28 08:31:27 -0700

    implements `/event` route: - fetcher logic - server data mutation logic - client-side React markup - route layout / wrapper

---
+++ app/routes/event.tsx
@@ -0,0 +1,25 @@
+import type { MetaFunction } from '@remix-run/node';
+import { Outlet } from '@remix-run/react';
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: 'Lb9_club' },
+    {
+      name: 'description',
+      content: 'A super secret event network for friends',
+    },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="flex h-dvh w-screen max-w-full overscroll-none px-2 py-4 md:px-0 md:py-0">
+      {/* The "leave by nine" banner */}
+      <div className="px-1 py-4 font-sans text-xl font-extralight text-[#867E7B] [writing-mode:vertical-rl]">
+        Leave by Nine club
+      </div>
+      {/* The content slot */}
+      <Outlet />
+    </div>
+  );
+}
